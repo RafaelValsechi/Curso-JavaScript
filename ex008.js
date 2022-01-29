32fab7c76192f87424c217747072ae3e230f203e
RafaelValsechi <rafaelvalsechi@gmail.com>
2022-01-29 01:05:49 -0300

    Aula 11

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A sua velocidade é ${vel} km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade! Você foi MULTADO!!!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
---
+++ ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Italia'
+console.log(`Se você vive no(a) ${pais}, você é `)
+if (pais == 'Brasil') {
+    console.log('Animal')
+} else {
+    console.log('Gente')
+}
